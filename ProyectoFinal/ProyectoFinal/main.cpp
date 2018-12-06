f4ae60af6ab5dff71ff3e24cd7aad36de27883d7
isaactellez <isaactb12@gmail.com>
2018-12-06 09:26:52 -0600

    Added comments

--- ProyectoFinal/ProyectoFinal/main.cpp
+++ ProyectoFinal/ProyectoFinal/main.cpp
@@ -9,7 +9,7 @@
  * Proyecto final de Gráficas por Computadora
  */
 
-//code based on ntu.edu.sg
+//Code based on ntu.edu.sg
 //http://www.ntu.edu.sg/home/ehchua/programming/opengl/CG_examples.html
 //#include <windows.h>  // for MS Windows
 #include <GLUT/glut.h>  // GLUT, include glu.h and gl.h
@@ -19,12 +19,12 @@
 using namespace std;
 
 // Global variables
-//title of the proyect
+//Title of the proyect
 char title[] = "Proyecto Final";
-//width and height of the window
+//Width and height of the window
 int w = 1440;
 int h = 900;
-//limits of scene
+//Limits of scene
 int xmin = -8;
 int xmax = 8;
 int ymin = -5;
@@ -32,21 +32,30 @@ int ymax = 5;
 int zmin = -20;
 int zmax = 0;
 int zint = -10;
-//level of transparency or scaled
+//Level of transparency and scale
 double levelS = 1;
 double levelT = 0;
-int refreshMills = 15;        // refresh interval in milliseconds [NEW]
+// Refresh interval in milliseconds
+int refreshMills = 15;
 GLboolean d = true;
-//booleans to know if a figure is selected
+//Booleans to know if a figure is to be moved or spinned
 bool isCubeMoving = false;
 bool isEllipsoidMoving = false;
 bool isSphereMoving = false;
 bool isCubeSpinning = false;
 bool isEllipsoidSpinnig = false;
 bool isSphereSpinning = false;
+//Character to indicate the figure to apply an action
+//Set on sphere by default
 char fig = 's';
 
-//structures with properties of figures
+//Structures with properties of figures
+//R, G, B.- Color of the figure
+//X, Y, Z.- Position of the figure
+//angle.- Current angle (not used)
+//trans.- Transparency value
+//large/radio.- Size-related property
+//dx, dy, dz, dt.- Represents the change in any of the given properties (x, y, z, orientation)
 struct cube {
     float colorR;
     float colorG;
@@ -93,10 +102,14 @@ struct ellipsoid {
     float dz;
     float dt;
 };
+
+//Vectors to keep all figures of the same type
 vector<cube> cubes;
 vector<sphere> spheres;
 vector<ellipsoid> ellipsoids;
 
+//Declaration of some methods to be used
+//C++ requires them to be declared before the first use
 void createCube();
 void createSphere();
 void createEllipsoid();
@@ -106,6 +119,9 @@ void setColors(char fig);
 void setScale(char fig, double s);
 void setTrans(char fig, double s);
 
+//Method to read keyboard input
+//Functionality is implemented as stated on the project requirements
+//The number keys need to be pressed before using either scale or transparency functions
 void keyboardPressed(unsigned char key, int x, int y)
 {
     switch (key) {
@@ -175,8 +191,11 @@ void keyboardPressed(unsigned char key, int x, int y)
     }
 }
 
+//Method to read mouse input
+//Used the position of the left menu figures to determine the type of figure each action will apply to
 void mouseClick(int button, int state, int x, int y)
 {
+    //Test prints to verify mouse clicks, including button pressed and (x,y) position
     cout << button;
     cout << state;
     cout << "x: " << x;
@@ -200,6 +219,7 @@ void mouseClick(int button, int state, int x, int y)
     
 }
 
+//Constructors for each type of figure
 cube newCube(float cR, float cG, float cB, float x, float y, float z, float t, float l) {
     cube myCube;
     myCube.colorR = cR;
@@ -253,12 +273,16 @@ ellipsoid newEllipsoid(float cR, float cG, float cB, float x, float y, float z, 
     return myEllipsoid;
 }
 
+//Drawing code for each type of figure
+//Sphere doesn't receive a theta (angle) value as spinning is not defined for this type
 void drawSphere(float x, float y, float z, float r) {
     r = (double)r;
     glTranslatef(x, y, z);
     glutSolidSphere(r, 50, 50);
 }
 
+//For the rotation of both ellipsoid and cube, a translation to the origin is performed first,
+//then the rotation and lastly it is placed on its right position
 void drawEllipsoid(float x, float y, float z, float t, float r) {
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
@@ -280,6 +304,9 @@ void drawCube(float x, float y, float z, float t, float l) {
     glPopMatrix();
 }
 
+//Generate random values for each figure parameter
+//Colors are generated in a range from 0.0 to 0.5
+//rX, rY and rZ are generated in a range of values inside the scene
 void createCube() {
     int rCR, rCG, rCB, rX, rY, rZ;
     //rCR = rand() % (2);
@@ -325,6 +352,11 @@ void createEllipsoid() {
     ellipsoids.push_back(e);
 }
 
+//Method that receives a character for determining which figure to apply movement to
+//Movement is set as specified on the project requirements
+//'c'.- Cubes only have x and y movement
+//'e'.- Ellipsoids have y and z movement
+//'s'.- Ellipsoids have movement on all axes
 void setMov(char fig) {
     if (fig == 'c') {
         isCubeMoving = !isCubeMoving;
@@ -355,6 +387,7 @@ void setMov(char fig) {
     
 }
 
+//Determine whether or not a figure is to spin
 void setRot(char fig) {
     
     if (fig == 'c') {
@@ -366,6 +399,7 @@ void setRot(char fig) {
     
 }
 
+//Method to randomize color on figures
 void setColors(char fig) {
     if (fig == 'c') {
         for (int i = 0;i<cubes.size();i++) {
@@ -390,6 +424,7 @@ void setColors(char fig) {
     }
 }
 
+//Method to scale figures depending on the number key pressed
 void setScale(char fig, double s) {
     if (fig == 'c') {
         for (int i = 0;i<cubes.size();i++) {
@@ -409,6 +444,7 @@ void setScale(char fig, double s) {
     }
 }
 
+//Method to scale figures depending on the number key pressed
 void setTrans(char fig, double s) {
     if (fig == 'c') {
         for (int i = 0;i<cubes.size();i++) {
@@ -428,6 +464,9 @@ void setTrans(char fig, double s) {
     }
 }
 
+//Mov methods are in charge of determining the direction of movement of the figures,
+//depending on its current position and direction
+//If its at the edge of the visible scene, it changes direction
 void movCube(cube &c) {
     
     if (c.positionX<xmin || c.positionX>xmax) {
@@ -475,6 +514,8 @@ void movSphere(sphere &s) {
     s.positionZ = s.positionZ + s.dz;
 }
 
+//Rot methods are in charge of determining the direction and magnitude of spin
+//It is always in one direction and the axes are determined in the "draw" methods
 void rotCube(cube &c) {
     
     c.dt = c.dt + rand() % (101)*0.1;
@@ -487,6 +528,8 @@ void rotEllipsoid(ellipsoid &e) {
     
 }
 
+//Method responsible for updating the movement parameters of every figure on the scene
+//Using glutTimerFunc, this method is being updated every 25 milliseconds
 void addDeltas(int x) {
     
     if (isCubeMoving) {
@@ -528,9 +571,9 @@ void initGL() {
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f); // Set background color to black and opaque
-    glClearDepth(1.0f);                   // Set background depth to farthest
-    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
     
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
@@ -539,7 +582,10 @@ void initGL() {
     glEnable(GL_COLOR_MATERIAL);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 }
-//code extracted from programming-techniques.com and adapted to our work
+
+//Code extracted from programming-techniques.com and adapted to our work
+//Receives a position (x,y) and a string to be displayed
+//Uses glutBitmapCharacter to generate a bitmap with the specified font and size
 //https://www.programming-techniques.com/2012/05/font-rendering-in-glut-using-bitmap.html
 void renderBitmapString(float x, float y, const char *string) {
     const char *c;
@@ -564,11 +610,10 @@ void resetPerspectiveProjection() {
     glPopMatrix();
     glMatrixMode(GL_MODELVIEW);
 }
-//end of extrenal code
+//end of external code
 
 /* Handler for window-repaint event. Called back when the window first appears and
  whenever the window needs to be re-painted. */
-
 void display() {
     float i;
     
@@ -581,6 +626,7 @@ void display() {
     glMatrixMode(GL_MODELVIEW);  // To operate on the Projection matrix
     glLoadIdentity();
     
+    //Lights position and color
     GLfloat ambientColor[] = { 0.2, 0.2, 0.2, 1.0 };
     GLfloat Color0[] = { 0.5f, 0.5f, 0.5f, 1.0f };
     GLfloat Position0[] = { 0.0f, 10.0f, 0.0f, 1.0f };
@@ -595,14 +641,14 @@ void display() {
     glLightfv(GL_LIGHT1, GL_DIFFUSE, Color1);
     glLightfv(GL_LIGHT1, GL_POSITION, Position1);
     
-    //viewport menu figures
+    //Viewport for left menu figures
     glViewport(0, 0, 300, h);
     GLfloat aspect = (GLfloat)300 / (GLfloat)(h);
     glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
     glLoadIdentity();             // Reset
     glOrtho(-1.5, 1.5, -1.5 / aspect, 1.5 / aspect, 0.1, 100);
     
-    //menu static figures
+    //Left menu static figures
     glColor3f(1.0f, 0.0f, 0.0f);
     drawEllipsoid(0.0f, 2.5f, -6.0f, 0, 1);
     glColor3f(0.0f, 1.0f, 0.0f);
@@ -610,7 +656,7 @@ void display() {
     glColor3f(0.0f, 0.0f, 1.0f);
     drawSphere(0.0f, -2.5f, -6.0f, 0.5);
     
-    //Viewport stripes scene
+    //Viewport for main scene
     glViewport(300, 150, w - 300, h - 150);
     GLfloat aspect1 = (GLfloat)(w - 300) / (GLfloat)(h - 150);
     
@@ -620,7 +666,7 @@ void display() {
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    //Ploting figures
+    //Plotting figures by iterating over figure vectors and getting every parameter needed
     for (int i = 0; i<cubes.size(); i++) {
         glColor4f(cubes[i].colorR, cubes[i].colorG, cubes[i].colorB, cubes[i].trans);
         glPushMatrix();
@@ -642,7 +688,7 @@ void display() {
         glPopMatrix();
     }
     
-    
+    //Code for black stripes
     //The black stripes are at z = -50
     glColor3f(0.0f, 0.0f, 0.0f);
     for (i = -9.0f; i < 9; i = i + 0.5) {
@@ -680,7 +726,7 @@ void display() {
     glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
 
-/* Called back when timer expired [NEW] */
+/* Called back when timer expired */
 void timer(int value) {
     glutPostRedisplay();      // Post re-paint request to activate display()
     glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
